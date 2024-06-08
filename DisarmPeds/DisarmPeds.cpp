077afd590a9222599d5171d34f8d900c5d28c65a
RahulM-3 <rahulcode97@gmail.com>
2024-06-08 17:16:20 +0530

    cache memory for debug

--- DisarmPeds/DisarmPeds.cpp
+++ DisarmPeds/DisarmPeds.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <vector>
 #include "hashtostr.h"
+#include <sstream>
+
+
+cachememory cacheset(10);
 
 void update()
 {
@@ -14,36 +18,48 @@ void update()
 	if (!ENTITY::DOES_ENTITY_EXIST(playerPed))
 		return;
 
-	// there are lots of entities in some places so we need to
-	// remove possibilty of text being drawn on top of another text
-	// thats why we will check distance between text on screen
-	std::vector<text_box_t> textOnScreen;
-
 	// get all peds
 	const int ARR_SIZE = 1024;
 	Ped peds[ARR_SIZE];
 	int count = worldGetAllPeds(peds, ARR_SIZE);
 
-	// let's track all animals
+
+
+
+	// debug
 	for (int i = 0; i < count; i++)
 	{
-		if (PED::IS_PED_HUMAN(peds[i]))
+		if (PED::IS_PED_HUMAN(peds[i]) && peds[i] != playerPed && PLAYER::IS_PLAYER_FREE_AIMING_AT_ENTITY(player, peds[i]))
 		{
-			std::string health = std::to_string(ENTITY::GET_ENTITY_HEALTH(peds[i]));
-			std::string model = dehash(ENTITY::GET_ENTITY_MODEL(peds[i]), "ped");
-
-			Vector3 v = ENTITY::GET_ENTITY_COORDS(peds[i], TRUE, FALSE);
-			Vector3 plv = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE, FALSE);
-			std::string dist = std::to_string(GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(plv.x, plv.y, plv.z, v.x, v.y, v.z, TRUE));
-			
-			std::vector<std::string> text = {health, model, dist};
-			entity_draw_info_add(peds[i], text);
+			cacheset.add(peds[i]);
 		}
 	}
+
+	if (!cacheset.isempty())
+	{
+		std::vector<Ped> debpeds = cacheset.getpeds();
+		for (int i = 0; i < debpeds.size(); i++)
+		{
+			std::string health = std::to_string(ENTITY::GET_ENTITY_HEALTH(debpeds[i]));
+			std::string model = dehash(ENTITY::GET_ENTITY_MODEL(debpeds[i]), "ped");
+
+			Vector3 v = ENTITY::GET_ENTITY_COORDS(debpeds[i], TRUE, FALSE);
+			Vector3 plv = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE, FALSE);
+
+			std::stringstream ss;
+			ss.precision(2);
+			ss << std::fixed << GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(plv.x, plv.y, plv.z, v.x, v.y, v.z, TRUE);
+			std::string dist = ss.str();
+
+			std::vector<std::string> text = { health, model, dist };
+			entity_draw_info_add(debpeds[i], text);
+		}
+	}
+	cacheset.update();
 }
 
 void main()
-{		
+{	
 	while (true)
 	{
 		update();
--- DisarmPeds/debug.h
+++ DisarmPeds/debug.h
@@ -1,13 +1,11 @@
 #include <string>
 #include <vector>
 #include "script.h"
+#include <ctime>
+#include <unordered_map>
+#include <fstream>
 
-struct text_box_t
-{
-	std::string text;
-	float x, y;
-	byte r, g, b, a;
-};
+std::ofstream logfile("DisarmPeds.log");
 
 void entity_draw_info_add(Entity entity, std::vector<std::string> text)
 {
@@ -18,10 +16,8 @@ void entity_draw_info_add(Entity entity, std::vector<std::string> text)
 		// draw bounds
 		if (x < 0.01 || y < 0.01 || x > 0.93 || y > 0.93)
 			return;
-		// get coords
-		Vector3 plv = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE, FALSE);
-		float dist = GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(plv.x, plv.y, plv.z, v.x, v.y, v.z, TRUE);
-		
+
+		float widthmul = 0, heightmul = text.size();
 		char formatedtext[256];
 		formatedtext[0] = '^';
 		int i = 1;
@@ -30,19 +26,79 @@ void entity_draw_info_add(Entity entity, std::vector<std::string> text)
 			formatedtext[i++] = '\n';
 			formatedtext[i++] = '|';
 			formatedtext[i++] = ' ';
+			widthmul = max(t.size(), widthmul);
 			for (int j = 0; j < t.size(); j++)
 			{
 				formatedtext[i++] = t[j];
 			}
 		}
-		//sprintf_s(formatedtext, "^\n| %s %08X\n| Distance %.02f\n| Health %d", type.c_str(), model, dist, health);
 
 		// draw
-		UI::SET_TEXT_SCALE(0.2, 0.2);
+		UI::SET_TEXT_SCALE(0.4, 0.4);
 		UI::SET_TEXT_COLOR_RGBA(255, 255, 255, 255);
 		UI::SET_TEXT_CENTRE(0);
 		UI::SET_TEXT_DROPSHADOW(0, 0, 0, 0, 0);
 		UI::DRAW_TEXT(GAMEPLAY::CREATE_STRING(10, "LITERAL_STRING", const_cast<char*>(formatedtext)), x, y);
-		GRAPHICS::DRAW_RECT(x + 0.028f, y + 0.033f, 0.058f, 0.041f, 75, 75, 75, 100, 0, 0);
 	}
 }
+
+class cachememory
+{
+private:
+	std::unordered_map<Ped, time_t> cacheset;
+	float cachetime;
+
+public:
+	cachememory(float time)
+	{
+		cachetime = time;
+	}
+
+	void add(Ped ped)
+	{
+		cacheset[ped] = std::time(0);
+	}
+
+	int update()
+	{
+		int removed = 0;
+		for (auto i = cacheset.begin(); i != cacheset.end(); i++)
+		{
+			if (std::time(0) - i->second >= cachetime)
+			{
+				removed++;
+				cacheset.erase(i);
+			}
+		}
+		return removed;
+	}
+
+	bool isempty()
+	{
+		if (cacheset.size() == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	std::vector<time_t> gettime()
+	{
+		std::vector<time_t> time;
+		for (auto i = cacheset.begin(); i != cacheset.end(); i++)
+		{
+			time.push_back(i->second);
+		}
+		return time;
+	}
+
+	std::vector<Ped> getpeds()
+	{
+		std::vector<Ped> peds;
+		for (auto i = cacheset.begin(); i != cacheset.end(); i++)
+		{
+			peds.push_back(i->first);
+		}
+		return peds;
+	}
+};
