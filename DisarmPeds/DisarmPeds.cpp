24e73c0c812e4b0e9a8a6ad3c97d777a83203673
RahulM-3 <rahulcode97@gmail.com>
2024-06-08 22:24:41 +0530

    debug information update

--- DisarmPeds/DisarmPeds.cpp
+++ DisarmPeds/DisarmPeds.cpp
@@ -4,9 +4,11 @@
 #include <vector>
 #include "hashtostr.h"
 #include <sstream>
+#include "bone.h"
 
 
 cachememory cacheset(10);
+int bone;
 
 void update()
 {
@@ -23,8 +25,17 @@ void update()
 	Ped peds[ARR_SIZE];
 	int count = worldGetAllPeds(peds, ARR_SIZE);
 
+	/*for (int i = 0; i < count; i++)
+	{
+		if (PED::IS_PED_HUMAN(peds[i]) && peds[i] != playerPed)
+		{
+			int bone;
+			if (PED::GET_PED_LAST_DAMAGE_BONE(peds[i], &bone))
+			{
 
-
+			}
+		}
+	}*/
 
 	// debug
 	for (int i = 0; i < count; i++)
@@ -51,7 +62,16 @@ void update()
 			ss << std::fixed << GAMEPLAY::GET_DISTANCE_BETWEEN_COORDS(plv.x, plv.y, plv.z, v.x, v.y, v.z, TRUE);
 			std::string dist = ss.str();
 
-			std::vector<std::string> text = { health, model, dist };
+			std::string gotshot = "None";
+			std::string anyarm = "None";
+			if (PED::GET_PED_LAST_DAMAGE_BONE(debpeds[i], &bone))
+			{
+				//logfile << mp_male__boneNames[bone] << "\n";
+				gotshot = mp_male__boneNames[bone];
+				anyarm = hand_bone_name[bone];
+			}
+
+			std::vector<std::string> text = {health, model, dist, gotshot, anyarm};
 			entity_draw_info_add(debpeds[i], text);
 		}
 	}
